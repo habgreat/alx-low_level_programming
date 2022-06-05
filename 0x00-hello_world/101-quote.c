261f10f12ffefed829685fa80e5412a416409dc9
Habtamu Debel <habtshdebel@gmail.com>
2022-06-05 05:59:52 -0700

    0x01-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,14 @@
-/*
-* File: 101-quote.c
-* Auth: Brennan D Baraban
-*/
-
+#include <stdio.h>
 #include <unistd.h>
 
 /**
-* main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
-* followed by a new line, to standard error.
-*
-* Return: Always 1.
-*/
-int main(void)	
+ *  * main - Entry point
+ *  *
+ *  * Return: Always 0 (Success)
+ */
+int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-59);
-return (1);
+ write(1, "and that piece of art is useful\" - Dora Korpar, 
+2015-10-19\n", 59);
+ return (1);
 }
