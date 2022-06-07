46f6d0961a48e4fc4fd6eea3542254f5970f38b4
Habtamu Debel <habtshdebel@gmail.com>
2022-06-07 10:50:43 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
-#include "main.c"
+#include "main.h"
 /**
  * print_alphabet - check the code for main
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 char alp = 'a';
 while (alp <= 'z')
